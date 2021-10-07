c17847adedcc5e9a39881a8ce2062f2ca00a9352
Rafael Chaidez <rafael.chaidez.r@gmail.com>
2021-10-07 15:03:43 -0600

    Se agrego el metodo de imprimir datos

--- traker.py
+++ traker.py
@@ -13,6 +13,11 @@ def rastrear(url):
 
     return datos
 
+def imprimir(dicc):
+    for x,y in dicc.items():
+        print(x,y)
+    print("")
+
 if __name__ == '__main__':
     laptop = rastrear("https://www.mercadolibre.com.mx/laptop-dell-inspiron-3505-gris-156-amd-ryzen-5-3450u-8gb-de-ram-256gb-ssd-amd-radeon-vega-8-60-hz-1366x768px-windows-10-home/p/MLM16537306?pdp_filters=category:MLM1652#searchVariation=MLM16537306&position=2&search_layout=stack&type=product&tracking_id=8defb765-9e97-4fe3-922a-be9e9b0fa052")
-    print(laptop)
+    imprimir(laptop)
