1a08d07beb23411e7f2b9d27ba38c2fe63baa4ff
Rafael Chaidez <rafael.chaidez.r@gmail.com>
2021-10-07 15:02:52 -0600

    Se volvio el codigo en una funcion y se llama por 'main'

--- traker.py
+++ traker.py
@@ -1,15 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
+def rastrear(url):
+    html =requests.get(url)
+    soup = BeautifulSoup(html.content, 'html.parser')
 
-url = "https://www.mercadolibre.com.mx/laptop-lenovo-ideapad-14igl05-ice-blue-14-intel-pentium-silver-n5030-4gb-de-ram-128gb-ssd-intel-uhd-graphics-605-1366x768px-windows-10-home/p/MLM16029714?pdp_filters=category:MLM1652#searchVariation=MLM16029714&position=1&search_layout=stack&type=product&tracking_id=602e052f-3a94-46f2-a689-d34b79167772"
-html =requests.get(url)
-soup = BeautifulSoup(html.content, 'html.parser')
+    datos = {}
+    datos["producto"] = soup.find("h1",class_="ui-pdp-title").get_text()
+    datos["precio"] = soup.find("span",class_="price-tag-fraction").get_text()
+    datos["diponibles"] = soup.find("span",class_="ui-pdp-buybox__quantity__available").get_text()
+    datos["msi"] = soup.find("div",class_="ui-pdp-price__subtitles").get_text()
 
-datos = {}
-datos["producto"] = soup.find("h1",class_="ui-pdp-title").get_text()
-datos["precio"] = soup.find("span",class_="price-tag-fraction").get_text()
-datos["diponibles"] = soup.find("span",class_="ui-pdp-buybox__quantity__available").get_text()
-datos["msi"] = soup.find("div",class_="ui-pdp-price__subtitles").get_text()
+    return datos
 
-print(datos)
+if __name__ == '__main__':
+    laptop = rastrear("https://www.mercadolibre.com.mx/laptop-dell-inspiron-3505-gris-156-amd-ryzen-5-3450u-8gb-de-ram-256gb-ssd-amd-radeon-vega-8-60-hz-1366x768px-windows-10-home/p/MLM16537306?pdp_filters=category:MLM1652#searchVariation=MLM16537306&position=2&search_layout=stack&type=product&tracking_id=8defb765-9e97-4fe3-922a-be9e9b0fa052")
+    print(laptop)
